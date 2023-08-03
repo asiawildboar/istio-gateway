6d54352b4ff26a0ef9ee77ae929c30858cffe157
0xwildboar <95584914+asiawildboar@users.noreply.github.com>
2023-08-03 03:39:09 -0700

    Update run.sh

--- istio-ingressgateway/run.sh
+++ istio-ingressgateway/run.sh
@@ -1,5 +1,5 @@
-kubectl apply -f https://github.com/asiawildboar/istio-samples/blob/main/istio-ingressgateway/gateway-base-resources.yaml
-kubectl apply -f https://github.com/asiawildboar/istio-samples/blob/main/istio-ingressgateway/k8s-resources.yaml
-kubectl apply -f https://github.com/asiawildboar/istio-samples/blob/main/istio-ingressgateway/basic-gateway-virtualservice.yaml
+kubectl apply -f https://raw.githubusercontent.com/asiawildboar/istio-samples/blob/main/istio-ingressgateway/gateway-base-resources.yaml
+kubectl apply -f https://raw.githubusercontent.com/asiawildboar/istio-samples/blob/main/istio-ingressgateway/k8s-resources.yaml
+kubectl apply -f https://raw.githubusercontent.com/asiawildboar/istio-samples/blob/main/istio-ingressgateway/basic-gateway-virtualservice.yaml
 
 curl -s -I -HHost:httpbin.example.com "http://$INGRESS_HOST:$INGRESS_PORT/status/200"
